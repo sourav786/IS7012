d865bbb0124402f19e7003867c8265791879d515
sourav786 <sourav77gupta@gmail.com>
2019-09-13 23:50:02 -0400

    First Web Page
    Consists of three web pages

---
+++ CoreWiki/Pages/Raut_Nacha.cshtml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CoreWiki.Pages
+{
+    public class Raut_NachaModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
